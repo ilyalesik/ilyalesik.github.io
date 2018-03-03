c2c502f78c9d99cdd30eab85737208e18c720aac
Ilya Lesik <ilialesik@gmail.com>
2018-03-03 20:15:43 +0300

    scene.js show text

--- scene.js
+++ scene.js
@@ -1,20 +1,58 @@
 
 
 function createScene(scene) {
-    var geometry	= new THREE.CubeGeometry(1,1,1);
-    var material	= new THREE.MeshNormalMaterial({
-        transparent : true,
-        opacity: 0.5,
-        side: THREE.DoubleSide
-    });
-    var mesh	= new THREE.Mesh( geometry, material );
-    mesh.position.y	= geometry.parameters.height/2
-    scene.add( mesh );
 
-    var geometry	= new THREE.TorusKnotGeometry(0.3,0.1,64,16);
-    var material	= new THREE.MeshNormalMaterial();
-    var mesh	= new THREE.Mesh( geometry, material );
-    mesh.position.y	= 0.5
-    scene.add( mesh );
+
+    var loader = new THREE.FontLoader();
+    loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+        var xMid, text;
+        var textShape = new THREE.BufferGeometry();
+        var color = 0x006699;
+        var matDark = new THREE.LineBasicMaterial( {
+            color: color,
+            side: THREE.DoubleSide
+        } );
+        var matLite = new THREE.MeshBasicMaterial( {
+            color: color,
+            transparent: true,
+            opacity: 0.4,
+            side: THREE.DoubleSide
+        } );
+        var message = "   Three.js\nSimple text.";
+        var shapes = font.generateShapes( message, 1, 2 );
+        var geometry = new THREE.ShapeGeometry( shapes );
+        geometry.computeBoundingBox();
+        xMid = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
+        geometry.translate( xMid, 0, 0 );
+        // make shape ( N.B. edge view not visible )
+        textShape.fromGeometry( geometry );
+        text = new THREE.Mesh( textShape, matLite );
+        text.position.y	= geometry.parameters.height/2;
+        scene.add( text );
+        // make line shape ( N.B. edge view remains visible )
+        var holeShapes = [];
+        for ( var i = 0; i < shapes.length; i ++ ) {
+            var shape = shapes[ i ];
+            if ( shape.holes && shape.holes.length > 0 ) {
+                for ( var j = 0; j < shape.holes.length; j ++ ) {
+                    var hole = shape.holes[ j ];
+                    holeShapes.push( hole );
+                }
+            }
+        }
+        shapes.push.apply( shapes, holeShapes );
+        var lineText = new THREE.Object3D();
+        for ( var i = 0; i < shapes.length; i ++ ) {
+            var shape = shapes[ i ];
+            var points = shape.getPoints();
+            var geometry = (new THREE.BufferGeometry()).setFromPoints( points );
+
+            geometry.translate( xMid, 0, 0 );
+            var lineMesh = new THREE.Line( geometry, matDark );
+            lineText.add( lineMesh );
+        }
+        scene.add( lineText );
+    } ); //end load function
+
 
 }
