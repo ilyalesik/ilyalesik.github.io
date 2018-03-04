c268f24fc8ff723c3a3909907c5ea9edf07b9848
Ilya Lesik <ilialesik@gmail.com>
2018-03-04 18:31:15 +0300

    solid text

--- scene.js
+++ scene.js
@@ -8,19 +8,14 @@ function createScene(scene) {
         var xMid, text;
         var textShape = new THREE.BufferGeometry();
         var color = 0x006699;
-        var color1 = 0xFF0000;
-        var matDark = new THREE.MeshBasicMaterial( {
-            color: color1,
-            side: THREE.DoubleSide
-        } );
         var matLite = new THREE.MeshBasicMaterial( {
             color: color,
             transparent: true,
-            opacity: 0.4,
+            opacity: 0.8,
             side: THREE.DoubleSide
         } );
         var message = "8 march";
-        var shapes = font.generateShapes( message, 0.5, 4 );
+        var shapes = font.generateShapes( message, 0.5, 20 );
         var geometry = new THREE.ShapeGeometry( shapes );
         geometry.computeBoundingBox();
         xMid = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
@@ -28,32 +23,8 @@ function createScene(scene) {
         // make shape ( N.B. edge view not visible )
         textShape.fromGeometry( geometry );
         text = new THREE.Mesh( textShape, matLite );
-        var h = geometry.parameters.height/2;
-        text.position.y	= h;
-        scene.add( text );
-        // make line shape ( N.B. edge view remains visible )
-        var holeShapes = [];
-        for ( var i = 0; i < shapes.length; i ++ ) {
-            var shape = shapes[ i ];
-            if ( shape.holes && shape.holes.length > 0 ) {
-                for ( var j = 0; j < shape.holes.length; j ++ ) {
-                    var hole = shape.holes[ j ];
-                    holeShapes.push( hole );
-                }
-            }
-        }
-        shapes.push.apply( shapes, holeShapes );
-        var lineText = new THREE.Object3D();
-        for ( var i = 0; i < shapes.length; i ++ ) {
-            var shape = shapes[ i ];
-            var points = shape.getPoints();
-            var geometry = (new THREE.BufferGeometry()).setFromPoints( points );
 
-            geometry.translate( xMid, 0, 0 );
-            var lineMesh = new THREE.Line( geometry, matDark );
-            lineText.add( lineMesh );
-        }
-        scene.add( lineText );
+        scene.add( text );
     } ); //end load function
 
 
